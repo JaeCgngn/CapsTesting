e081dceaadeb0b18349b28d71908eb7749ae7f85
JaeCgngn <jaecgngn@gmail.com>
2026-06-12 18:36:51 +0800

    added 3d model and scriptable object

---
+++ Assets/Scripts/TattooSamp.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TattooSamp", menuName = "Scriptable Objects/TattooSamp")]
+public class TattooSamp : ScriptableObject
+{
+     public string tattooID;
+    public Texture2D tattooTexture;
+}
